172e67d0b79390366d61c5ff00c693ab77cc3fa3
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-11 07:45:33 +0100

    redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit"
+import employeesReducer from './employeesSlice'
+
+export const store = configureStore({
+    reducer: {
+        messaging : employeesReducer,
+    },
+    devTools : true,
+  })
+
+// export those types so they can be used globally : to type some redux hooks
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export default store
