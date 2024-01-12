1fe03928f4f6e94fefa8d99decd494a9a1d7bd63
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-12 06:28:18 +0100

    darkpreset

---
+++ src/components/DatasTable/presets/darkPreset.ts
@@ -0,0 +1,42 @@
+const darkPreset = {
+    global :{
+        font : "Jost",
+        textColor : "#FFFFFF",
+    },
+    th : {
+        textColor : "#FFFFFF",
+        fontWeight: "500",
+        backgroundColor : "#000000",
+        arrowInactiveColor : "#666666",
+        arrowActiveColor : "#FFFFFF",
+        separatorColor : "#999999"
+    },
+    evenRow : {
+        backgroundColor : "#000000",
+        textColor : "#FFFFFF",
+        fontWeight: "500",
+        hoverBackgroundColor : "#555555",
+        bottomSeparatorColor : "#444444",
+    },
+    oddRow : {
+        backgroundColor : "#333333",
+        textColor : "#FFFFFF",
+        fontWeight: "500",
+        hoverBackgroundColor : "#555555",
+        bottomSeparatorColor : "#444444",
+    },
+}
+
+
+    /*"font" : "Jost",
+    "textColor" : "#FFFFFFFF",
+    "ThTextFontWeight" : "",
+    "ThArrowColor" : "",
+    "ThActiveArrowColor" : "",
+    "evenRowBgColor" : "",
+    "evenRowHoverBgColor" : "",
+    "oddRowBgColor" : "",
+    "oddRowHoverBgColor" : "",
+    "rowTextHoverColor" : "",
+    "rowSeparatorColor" : "",
+    "firstnLastRowSeparatorColor" : "",*/
