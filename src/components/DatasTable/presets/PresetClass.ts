308fa95c3151443f5195d500ed1e973d4bb4e914
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-24 10:58:36 +0100

    preset class jsdoc

--- src/components/DatasTable/presets/PresetClass.ts
+++ src/components/DatasTable/presets/PresetClass.ts
@@ -7,19 +7,34 @@ class PresetClass {
         this.#preset = preset
     }
 
+    /**
+     * @private
+     * @param {IPreset} preset - The preset object
+     */
     #set(preset : IPreset){
         this.#preset = preset
     }
     
+    /**
+     * @returns {IPreset} The preset object
+     */
     get(){
         return this.#preset
     }
 
+    /**
+     * @param {string} font - The global font
+     * @returns {PresetClass}
+     */
     setGlobalFont (font: string): PresetClass {
         this.#set({ ...this.#preset, global: { ...this.#preset.global, font: font } } as IPreset)
         return this
     }
 
+    /**
+     * @param {{ _default: string; focus: string; }} colors - The default & the focus border colors of the searchbar / dropdown
+     * @returns {PresetClass}
+     */
     setBordersColors ({ _default, focus }: { _default: string; focus: string; }): PresetClass {
         this.#set({ ...this.#preset, 
             selectEntriesPerPage: { ...this.#preset.selectEntriesPerPage, selectBorderColor: { default: _default, focus } },
@@ -28,6 +43,10 @@ class PresetClass {
         return this
     }
 
+    /**
+     * @param {string} color - The first & last separators color
+     * @returns {PresetClass}
+     */
     setSeparatorColor (color: string): PresetClass {
         this.#set({ ...this.#preset, 
             firstnLastRowSeparatorsColor : color
@@ -35,6 +54,10 @@ class PresetClass {
         return this
     }
 
+    /**
+     * @param {{ textColor: string; background: string; arrowColor: string; activeArrowColor: string; }} style - The table header style
+     * @returns {PresetClass}
+     */
     setTHStyle ({ textColor, background, arrowColor, activeArrowColor }: { textColor: string; background: string; arrowColor: string; activeArrowColor: string; }): PresetClass {
         this.#set({ ...this.#preset, 
             th : {...this.#preset.th,
@@ -50,6 +73,10 @@ class PresetClass {
         throw new Error("Function not implemented." + textColor + background);
     }
 
+    /**
+     * @param {{ background: string; separatorColor: string; }} style - The odd rows style
+     * @returns {PresetClass}
+     */
     setOddRowsStyle ({ background, separatorColor }: { background: string; separatorColor: string; }): PresetClass {
         this.#set({ ...this.#preset, 
             oddRow : {...this.#preset.oddRow,
@@ -60,6 +87,10 @@ class PresetClass {
         return this
     }
 
+    /**
+     * @param {{ background: string; separatorColor: string; }} style - The even rows style
+     * @returns {PresetClass}
+     */
     setEvenRowsStyle ({ background, separatorColor }: { background: string; separatorColor: string;}): PresetClass {
         this.#set({ ...this.#preset, 
             evenRow : {...this.#preset.evenRow,
